e327b2720e36ec4f3dc15776d2c85e068b611779
Adonissys <adonisfilipe1@gmail.com>
2026-08-06 19:59:55 -0300

    Adding new scenes and buttons to main
    Update

---
+++ club-club-jam-jam-/Scripts/control.gd
@@ -0,0 +1,34 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for button in get_tree().get_nodes_in_group("Button"):
+		if (button is Button):
+			button.pressed.connect(on_button_pressed.bind(button))
+			button.mouse_exited.connect(mouse_interaction.bind(button,"exited"))
+			button.mouse_entered.connect(mouse_interaction.bind(button,"entered"))
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func on_button_pressed(button: Button) -> void:
+	match button.name:
+		"VelocityMinigame":
+			get_tree().change_scene_to_file("res://Scenes/VelocityMinigame.tscn")
+		"IntegrityMinigame":
+			get_tree().change_scene_to_file("res://Scenes/IntegrityMinigame.tscn")
+		"ReactionMinigame":
+			get_tree().change_scene_to_file("res://Scenes/ReactionMinigame.tscn")
+		"StoreButton":
+			get_tree().change_scene_to_file("res://Scenes/StoreScene.tscn")
+		"PlayButton":
+			get_tree().change_scene_to_file("res://Scenes/CourseScene.tscn")
+		
+
+func mouse_interaction(button: Button, state: String) -> void:
+	pass
