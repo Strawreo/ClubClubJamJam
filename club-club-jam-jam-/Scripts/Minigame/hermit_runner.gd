1b44924ea107ce8c4ddd8df6151b122b7df9d80d
Strawreo <strawreomilk@gmail.com>
2026-08-07 10:28:24 -0300

    Minigame End Menu (Game Over)

--- club-club-jam-jam-/Scripts/Minigame/hermit_runner.gd
+++ club-club-jam-jam-/Scripts/Minigame/hermit_runner.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED = 200.0
 const MAX_JUMP_VELOCITY = -300.0
 const MIN_JUMP_VELOCITY = -200.0
+const GAME_OVER= preload("res://Scenes/Menu/MinigameEndMenu.tscn")
 
 
 func _physics_process(delta: float) -> void:
@@ -12,15 +13,19 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = MIN_JUMP_VELOCITY
 	if Input.is_action_pressed("up") and velocity.y > MAX_JUMP_VELOCITY:
-		velocity.y -= 8
+		velocity.y -= 10
 
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	#var direction := Input.get_axis("left", "right")
+	#if direction:
+		#velocity.x = direction * SPEED
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 
 func _on_death_area_area_entered(area: Area2D) -> void:
+	hide()
+	var game_over= GAME_OVER.instantiate()
+	get_parent().add_child(game_over)
 	queue_free()
+	
--- club-club-jam-jam-/Scripts/Minigame/velocity_minigame.gd
+++ club-club-jam-jam-/Scripts/Minigame/velocity_minigame.gd
@@ -1,20 +1,13 @@
 extends Node2D
+const OBSTACLE_SCENE= preload("res://Scenes/Minigame/Obstacle.tscn")
 
-const ObstacleScene = preload("res://Scenes/Minigame/Obstacle.tscn")
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	print("Spawn!")
 	$ObstacleTimer.wait_time = randi_range(2,8)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_obstacle_timer_timeout() -> void:
-	var obstacle = ObstacleScene.instantiate()
+	var obstacle = OBSTACLE_SCENE.instantiate()
 	obstacle.global_position = $ObstacleSpawn.global_position
 	obstacle.type = randi_range(0,1)
 	$ObstacleAreas.add_child(obstacle)
