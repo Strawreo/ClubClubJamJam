bb98c85f1f0f813cb931e5c1ed55bb989cc6aa87
Lucas8564 <214283272+Lucas8564@users.noreply.github.com>
2026-08-07 14:20:59 -0300

    Add Action_Button

--- club-club-jam-jam-/Scripts/Minigame/hermit_runner.gd
+++ club-club-jam-jam-/Scripts/Minigame/hermit_runner.gd
@@ -10,9 +10,9 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("Action_Button") and is_on_floor():
 		velocity.y = MIN_JUMP_VELOCITY
-	if Input.is_action_pressed("up") and velocity.y > MAX_JUMP_VELOCITY:
+	if Input.is_action_pressed("Action_Button") and velocity.y > MAX_JUMP_VELOCITY:
 		velocity.y -= 10
 
 	#var direction := Input.get_axis("left", "right")
